2c52f5beec333a562533935a039616f378e2506c
Scharif <scharif47@outlook.de>
2022-01-03 09:10:41 +0100

    widget design added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,20 @@ import styled from "styled-components";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const BurgerMenu = styled.div`
-  position: absolute;
-  top: 5px;
-  left: 5px;
-  padding: 0;
   border: 2px solid black;
   cursor: pointer;
   z-index: 900;
+`;
+
+const HeaderBar = styled.div`
+  position: absolute;
+  top: 0;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 0.5rem 0.5rem 0.5rem;
+  box-shadow: 0.2rem 0.3rem 0.4rem rgba(0, 0, 0, 0.4);
 `;
 
 function Header() {
@@ -25,8 +32,11 @@ function Header() {
   );
 
   return (
-    <header className=" mb-20 ">
-      <BurgerMenu onClick={BurgerMenuHandler}>{renderIcon}</BurgerMenu>
+    <header className="h-20">
+      <HeaderBar>
+        <BurgerMenu onClick={BurgerMenuHandler}>{renderIcon}</BurgerMenu>
+        <p className="">CryptoCoin</p>
+      </HeaderBar>
 
       <Navbar isOpen={isOpen} />
     </header>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,116 @@
 import React from "react";
+import styled from "styled-components";
+
 import Widget from "../components/Widget";
 import Infobar from "../components/Infobar";
 import Itemlist from "../components/Itemlist";
 import Pagination from "../components/Pagination";
+import cryptoWidget from "../images/crypto-widget.jpg";
+
+const InfoWidget = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: whitesmoke;
+  width: 16rem;
+  border-radius: 1rem;
+  padding-right: 0.5rem;
+
+  @media (min-width: 640px) {
+    flex-direction: column;
+    padding-right: 0;
+    width: 21rem;
+    height: 13rem;
+  }
+`;
 
 function Home() {
   return (
     <div>
+      <section className="m-auto w-60 sm:w-80 md:w-[30rem] mb-10">
+        <h1 className="mb-2">Welcome to CryptoCoin</h1>
+        <h2>Check crypto prices, news and learn about cryptocurrency.</h2>
+      </section>
+
+      <section className="flex space-x-12 sm:space-x-16 mb-20 px-6   whitespace-nowrap overflow-x-scroll">
+        <a href="/">
+          <InfoWidget>
+            <img
+              className="w-32 sm:w-full sm:h-36 h-full rounded-lg sm:rounded-xl"
+              alt="crypto coins"
+              src={cryptoWidget}
+            />
+            <div className=" sm:ml-0 sm:w-48 sm:text-center">
+              <p>Learn BTC</p>
+              <p>what is BTC?</p>
+            </div>
+          </InfoWidget>
+        </a>
+
+        <a href="/">
+          <InfoWidget>
+            <img
+              className="w-32 sm:w-full sm:h-36 h-full rounded-lg sm:rounded-xl"
+              alt="crypto coins"
+              src={cryptoWidget}
+            />
+            <div className=" sm:ml-0 sm:w-48 sm:text-center">
+              <p>Learn BTC</p>
+              <p>what is BTC?</p>
+            </div>
+          </InfoWidget>
+        </a>
+
+        <a href="/">
+          <InfoWidget>
+            <img
+              className="w-32 sm:w-full sm:h-36 h-full rounded-lg sm:rounded-xl"
+              alt="crypto coins"
+              src={cryptoWidget}
+            />
+            <div className=" sm:ml-0 sm:w-48 sm:text-center">
+              <p>Learn BTC</p>
+              <p>what is BTC?</p>
+            </div>
+          </InfoWidget>
+        </a>
+
+        <a href="/">
+          <InfoWidget>
+            <img
+              className="w-32 sm:w-full sm:h-36 h-full rounded-lg sm:rounded-xl"
+              alt="crypto coins"
+              src={cryptoWidget}
+            />
+            <div className=" sm:ml-0 sm:w-48 sm:text-center">
+              <p>Learn BTC</p>
+              <p>what is BTC?</p>
+            </div>
+          </InfoWidget>
+        </a>
+
+        <a href="/">
+          <InfoWidget>
+            <img
+              className="w-32 sm:w-full sm:h-36 h-full rounded-lg sm:rounded-xl"
+              alt="crypto coins"
+              src={cryptoWidget}
+            />
+            <div className=" sm:ml-0 sm:w-48 sm:text-center">
+              <p>Learn BTC</p>
+              <p>what is BTC?</p>
+            </div>
+          </InfoWidget>
+        </a>
+      </section>
 
       <section>
-        <Widget />
-        <Widget />
-        <Widget />
+        <h1 className="text-center mb-10">Check out the latest trends.</h1>
+        <div className="flex justify-around">
+          <Widget />
+          <Widget />
+          <Widget />
+        </div>
       </section>
 
       <main>
@@ -19,7 +118,6 @@ function Home() {
         <Itemlist />
         <Pagination />
       </main>
-
     </div>
   );
 }
